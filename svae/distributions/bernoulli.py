7ad15ff91b8074f8d08baa0b9bd3891df8ab7359
Jonathan So <jonny.so@gmail.com>
2018-10-13 22:55:48 +0100

    Improve numerical stability of bernoulli.logZ

--- svae/distributions/bernoulli.py
+++ svae/distributions/bernoulli.py
@@ -1,10 +1,12 @@
 import autograd.numpy as np
+import autograd.scipy as scipy
+from svae.util import log1pexp
 
 def expectedstats(x):
-    return 1./(1 + np.exp(-x))
+    return scipy.special.expit(x)
 
 def mean_to_natural(x):
-    return np.log(x/(1 - x))
+    return scipy.special.logit(x)
 
 def logZ(natparam):
-    return natparam + np.log(1 + np.exp(-natparam))
+    return np.where(natparam > 0, natparam + log1pexp(-natparam), log1pexp(natparam))
