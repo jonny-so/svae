58c7722078c12559172d90f009094406b5cee96b
Jonathan So <jonny.so@gmail.com>
2018-10-13 10:23:09 +0100

    added figure saving to binary_lds

--- experiments/binary_lds.py
+++ experiments/binary_lds.py
@@ -9,7 +9,9 @@ from toolz import identity
 import autograd.numpy as np
 import autograd.numpy.random as npr
 import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
 import scipy.stats
+import pickle
 
 def likelihood(scale, x):
     # return .25*(np.sin(x*scale))+.5
@@ -90,7 +92,63 @@ def validation_error(global_natparams, encoder, phi, decoder, gamma, y, n_sample
 
     p_pred = np.mean(sigmoid(decoder(gamma, x_pred[:,:,None])), axis=0)
 
-    return np.sum(y[:,-1]*np.log(p_pred) + (1-y[:,-1])*np.log(1-p_pred))
+    return -np.mean(y[:,-1]*np.log(p_pred) + (1-y[:,-1])*np.log(1-p_pred))
+
+def save_likelihood_plot(fileprefix, scale, y, ):
+
+    fig, ax = plt.subplots(1, 1, figsize=(4*4, 4))
+    ax.set_xlim(-3,3)
+    ax.set_ylim(0,1.1)
+    ax.set_aspect('equal')
+    ax.autoscale(False)
+    ax.yaxis.set_major_locator(ticker.MultipleLocator(0.5))
+
+    xs = np.linspace(-4., 4, 200)
+    if y == 1:
+        plot_or_update(0, ax, xs, likelihood(scale, xs), linestyle='-', linewidth=2, color='g')
+    else:
+        plot_or_update(0, ax, xs, 1 - likelihood(scale, xs), linestyle='-', linewidth=2, color='g')
+
+    filename = fileprefix + '_likelihood_' + 'y'+ str(y) + '_.png'
+    fig.savefig(filename, dpi=300, bbox_inches='tight', pad_inches=0)
+    print 'saved {}'.format(filename)
+    plt.close('all')
+
+def save_encoder_decoder_plot(fileprefix, encoder, phi, decoder, gamma, y):
+
+    fig, ax = plt.subplots(1, 1, figsize=(4*4, 4))
+    ax.set_xlim(-3,3)
+    ax.set_ylim(0,1.1)
+    ax.set_aspect('equal')
+    ax.autoscale(False)
+    ax.yaxis.set_major_locator(ticker.MultipleLocator(0.5))
+
+    encoder_out = encoder(phi, y*np.ones((1,1,1)))
+
+    x_encV, x_enc = univariate_gaussian.natural_to_standard(
+        univariate_gaussian.pack_dense(np.squeeze(encoder_out[0], -1), np.squeeze(encoder_out[1], -1)))
+    x_encW = softmax(encoder_out[2])
+
+    xs = np.linspace(-4., 4, 200)
+    normalize = lambda _: _/np.max(_)
+
+    if y == 1:
+        plot_or_update(0, ax, xs, sigmoid(decoder(gamma, xs[..., None])), linestyle='-', linewidth=2, color='r')
+    else:
+        plot_or_update(0, ax, xs, 1-sigmoid(decoder(gamma, xs[..., None])), linestyle='-', linewidth=2, color='r')
+
+    encoder_pdf = np.zeros(xs.shape)
+
+    for i in xrange(x_encW.shape[-1]):
+        encoder_dist = scipy.stats.norm(loc=x_enc[0,0,i], scale=np.sqrt(x_encV[0,0,i]))
+        encoder_pdf = encoder_pdf + encoder_dist.pdf(xs)*x_encW[0,0,i]
+
+    ax.fill_between(xs, np.zeros(xs.shape), normalize(encoder_pdf), facecolor='blue', alpha=0.5)
+
+    filename = fileprefix + '_encoder_decoder_' + 'y'+ str(y) + '_.png'
+    fig.savefig(filename, dpi=300, bbox_inches='tight', pad_inches=0)
+    print 'saved {}'.format(filename)
+    plt.close('all')
 
 def make_plotter(encoder, decoder, global_prior_natparams, scale, x, y):
     T, K, _ = y.shape
@@ -116,23 +174,21 @@ def make_plotter(encoder, decoder, global_prior_natparams, scale, x, y):
         plot_time_series(global_natparams, encoder, phi, x, y, 0, axes[0,1])
         plot_time_series(global_natparams, encoder, phi, x, y, 1, axes[1,1])
         plot_time_series(global_natparams, encoder, phi, x, y, 2, axes[2,1])
-        if iter % 10 == 0:
-            err = validation_error(global_natparams, encoder, phi, decoder, gamma, y)
-            print('{}: elbo={}, err={}'.format(iter, val, err))
         plt.pause(0.1)
 
     return plot
 
-if __name__ == "__main__":
+def run_experiment(C, seed):
     # use constant data set
-    npr.seed(0)
+    npr.seed(seed)
 
+    n_iter = 2000
     N = 10000
     T = 100
-    C = 2
     tau = 100.0
     scale = .5
     x, y = gen_data(N, T, tau, scale)
+    x_valid, y_valid = gen_data(1000, T+1, tau, scale)
 
     # global prior and variational posterior natparams
     global_prior_natparams = init_pgm_natparams(tau, np.power(20.0, 2))
@@ -152,12 +208,38 @@ if __name__ == "__main__":
 
     params = global_natparams, gamma, phi
 
-    plotter = make_plotter(encoder, decoder, global_prior_natparams, scale, x, y)
+    plot = make_plotter(encoder, decoder, global_prior_natparams, scale, x, y)
+    elbos = np.zeros(n_iter//10)
+    errs = np.zeros(n_iter//10)
+
+    def callback(i, val, params, grad):
+        if i % 10 == 0:
+            plot(i, val, params, grad)
+            err = validation_error(params[0], encoder, phi, decoder, gamma, y_valid, 100)
+            print('{}: elbo={}, err={}'.format(i, val, err))
+            elbos[i // 10] = val
+            errs[i // 10] = err
 
     np.seterr(all='raise')
 
     gradfun = make_gradfun(
         local_inference, encoder, loglike, global_prior_natparams, pgm_expectedstats, y)
 
-    params = adam(gradfun(batch_size=50, num_samples=3, natgrad_scale=1e4, callback=plotter),
-                  params, num_iters=5000, step_size=1e-3)
+    params = adam(gradfun(batch_size=50, num_samples=3, natgrad_scale=1e4, callback=callback),
+                  params, num_iters=n_iter, step_size=1e-3)
+
+    fileprefix = 'binary_lds/binary_lds_c' + str(C) + '_s' + str(seed)
+    pickle.dump(params, open(fileprefix + '_params.pkl', 'wb'))
+    save_encoder_decoder_plot(fileprefix, encoder, phi, decoder, gamma, 0)
+    save_encoder_decoder_plot(fileprefix, encoder, phi, decoder, gamma, 1)
+    save_likelihood_plot(fileprefix, scale, 0)
+    save_likelihood_plot(fileprefix, scale, 1)
+    np.savetxt(fileprefix + '_elbo.txt', elbos)
+    np.savetxt(fileprefix + '_errs.txt', errs)
+
+if __name__ == "__main__":
+    run_experiment(2, 0)
+    run_experiment(2, 1)
+    run_experiment(2, 2)
+    run_experiment(2, 3)
+    run_experiment(2, 4)
